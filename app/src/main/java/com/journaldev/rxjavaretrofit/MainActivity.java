67d0234e0d884a13096f83b163ff541cbec007eb
John Young <ynyjcxyz@gmail.com>
2022-01-15 23:27:11 -0800

    original edition

---
+++ app/src/main/java/com/journaldev/rxjavaretrofit/MainActivity.java
@@ -0,0 +1,121 @@
+package com.journaldev.rxjavaretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.journaldev.rxjavaretrofit.pojo.Crypto;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.journaldev.rxjavaretrofit.CryptocurrencyService.BASE_URL;
+
+public class MainActivity extends AppCompatActivity {
+    TextView time_stamp;
+    RecyclerView recyclerView;
+    Retrofit retrofit;
+    RecyclerViewAdapter recyclerViewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        time_stamp = findViewById(R.id.time_stamp);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewAdapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+
+        callEndpoints();
+    }
+
+    private void callEndpoints() {
+        CryptocurrencyService cryptocurrencyService = retrofit.create(CryptocurrencyService.class);
+
+        //Single call
+/*        Observable<Crypto> cryptoObservable = cryptocurrencyService.getCoinData("btc");
+        cryptoObservable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(result -> result.ticker)
+                .subscribe(this::handleResults, this::handleError);*/
+
+        Observable<List<Crypto.Market>> btcObservable =
+                cryptocurrencyService.getCoinData("btc")
+                .map(result -> Observable.fromIterable(result.ticker.markets))
+                .flatMap(x -> x)
+                        .filter(y -> {
+                            y.coinName = "btc";
+                            return true;
+                        })
+                        .toList()
+                        .toObservable();
+
+        Observable<List<Crypto.Market>> ethObservable =
+                cryptocurrencyService.getCoinData("eth")
+                .map(result -> Observable.fromIterable(result.ticker.markets))
+                .flatMap(x -> x)
+                        .filter(y -> {
+                            y.coinName = "eth";
+                            return true;
+                        })
+                        .toList()
+                        .toObservable();
+
+        Observable.merge(btcObservable, ethObservable)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResults, this::handleError);
+    }
+
+
+    private void handleResults(List<Crypto.Market> marketList) {
+        if (marketList != null && marketList.size() != 0) {
+            recyclerViewAdapter.setData(marketList);
+
+        } else {
+            Toast.makeText(this, "NO RESULTS FOUND",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void handleError(Throwable t) {
+        Toast.makeText(this, "ERROR IN FETCHING API RESPONSE. Try again",
+                Toast.LENGTH_LONG).show();
+    }
+
+}
