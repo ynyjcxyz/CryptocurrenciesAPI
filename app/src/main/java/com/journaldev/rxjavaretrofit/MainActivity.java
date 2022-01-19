bc50e1421077701fb1f0739128fb880cc4b51fc0
TonyTandAndroid <tonythompsoncmu@gmail.com>
2022-01-18 20:43:56 -0800

    Merge and sort.

--- app/src/main/java/com/journaldev/rxjavaretrofit/MainActivity.java
+++ app/src/main/java/com/journaldev/rxjavaretrofit/MainActivity.java
@@ -12,17 +12,25 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.journaldev.rxjavaretrofit.pojo.CoinMarket;
 import com.journaldev.rxjavaretrofit.pojo.CryptoDataModel;
 import com.journaldev.rxjavaretrofit.pojo.ServerCoinModel;
+import com.journaldev.rxjavaretrofit.pojo.ZippedCryptoDataModel;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import java.util.Comparator;
+import java.util.List;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * refresh the coin for btc and etc for every 3 seconds and update it in descending (biggest to smallest) volume order.
+ */
 public class MainActivity extends AppCompatActivity {
     TextView time_stamp;
     RecyclerView recyclerView;
@@ -62,11 +70,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void callEndpoints() {
         //Single call
-        Observable.merge(coinStream("btc"),coinStream("eth"))
+        Observable.zip(coinStream("btc"), coinStream("eth"), this::zip)
             .observeOn(AndroidSchedulers.mainThread())
-                .as(autoDisposable(from(this)))
-                .subscribe(this::handleResults, this::handleError);
+            .as(autoDisposable(from(this)))
+            .subscribe(this::handleResults, this::handleError);
 
+    }
+
+    private ZippedCryptoDataModel zip(CryptoDataModel btc, CryptoDataModel eth) {
+
+        long latest =Math.max(btc.serverCoinModel.timestamp,eth.serverCoinModel.timestamp);
+        List<CoinMarket> coinMarkets = mergeAndSort(btc, eth);
+        return new ZippedCryptoDataModel(latest,coinMarkets);
+
+    }
+
+    private List<CoinMarket> mergeAndSort(CryptoDataModel btc, CryptoDataModel eth) {
+        Observable<CoinMarket> btcStream = coinMarketStream(btc);
+        Observable<CoinMarket> ethStream = coinMarketStream(eth);
+        return Observable.merge(btcStream, ethStream)
+            .sorted(new Comparator<CoinMarket>() {
+                @Override
+                public int compare(CoinMarket o1, CoinMarket o2) {
+                    return Float.compare(o2.market.volume, o1.market.volume);
+                }
+            }).toList().blockingGet();
+    }
+
+    private Observable<CoinMarket> coinMarketStream(CryptoDataModel btc) {
+        return Observable.fromIterable(btc.serverCoinModel.markets)
+            .map(market -> new CoinMarket(btc.coinName, market));
     }
 
     private Observable<CryptoDataModel> coinStream(String coinName) {
@@ -80,9 +113,9 @@ public class MainActivity extends AppCompatActivity {
     //CryptoDataModel -> eth
     //CryptoDataModel -> btc
 
-    private void handleResults(CryptoDataModel model) {
+    private void handleResults(ZippedCryptoDataModel model) {
         recyclerViewAdapter.setData(model);
-        time_stamp.setText(String.valueOf(model.serverCoinModel.timestamp));
+        time_stamp.setText(String.valueOf(model.timestamp));
     }
 
 
